ec1637ee991dbd670d31fb5da3839c825d8f30fb
Ian Watts <iwatts@me.com>
2021-03-03 12:14:06 +0000

    Updated with Bootstrap

--- dashapp.py
+++ dashapp.py
@@ -7,7 +7,7 @@ from dash.dependencies import Input, Output
 import plotly.express as px
 import pandas as pd
 
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
 server = app.server
 
 mydb = mysql.connector.connect(
